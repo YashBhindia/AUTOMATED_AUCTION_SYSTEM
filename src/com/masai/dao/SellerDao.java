cc9d42955de4dcae61d919679ecdaaea72e4de17
YashBhindia <yashbhindia123@gmail.com>
2022-09-30 15:43:18 +0530

    Updated SellerDao

--- src/com/masai/dao/SellerDao.java
+++ src/com/masai/dao/SellerDao.java
@@ -1,5 +1,22 @@
 package com.masai.dao;
 
+import java.util.List;
+
+import com.masai.model.Product;
+import com.masai.model.Seller;
+
 public interface SellerDao {
 
+	public Seller registerSeller(Seller seller) throws SellerException;
+	
+	public List<Product> createListOfitems(List<Product> products) throws SellerException;
+	
+	public Product updateProduct(Product product) throws Sellerexception;
+	
+	public Product addProductInList(Product product) throws SellerException;
+	
+	public Product removeProductInList(Product product) throws SellerException;
+	
+	public List<Product> soldItems(int sellerId) throws SellerException;
+	
 }
