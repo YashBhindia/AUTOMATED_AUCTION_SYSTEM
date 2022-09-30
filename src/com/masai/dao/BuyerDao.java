f5901ea9d458782e5b612065970e2b09d37153c4
YashBhindia <yashbhindia123@gmail.com>
2022-09-30 15:32:49 +0530

    Updated BuyerDao

--- src/com/masai/dao/BuyerDao.java
+++ src/com/masai/dao/BuyerDao.java
@@ -1,5 +1,17 @@
 package com.masai.dao;
 
+import java.util.List;
+
+import com.masai.model.Buyer;
+import com.masai.model.Product;
+
 public interface BuyerDao {
 
+	public Buyer registerBuyer(Buyer buyer) throws BuyerException;
+	
+	public List<Buyer> viewAllBuyers() throws BuyerExcetion;
+	
+	public Product buyProduct(int productId) throws BuyerException; 
+	
+	
 }
