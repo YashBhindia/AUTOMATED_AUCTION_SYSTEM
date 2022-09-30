3d069dc01520718a035b4b69cca4821e572d3a5c
YashBhindia <yashbhindia123@gmail.com>
2022-09-30 15:47:22 +0530

    Updated Exception classes

---
+++ src/com/masai/exceptions/BuyerException.java
@@ -0,0 +1,15 @@
+package com.masai.exceptions;
+
+public class BuyerException extends Exception{
+
+	public BuyerException(){
+		
+	}
+	
+	public BuyerException(String message){
+		super(message);
+	}
+	
+	
+	
+}
---
+++ src/com/masai/exceptions/SellerException.java
@@ -0,0 +1,13 @@
+package com.masai.exceptions;
+
+public class SellerException extends Exception{
+
+	public SellerException() {
+		
+	}
+	
+	public SellerException(String message) {
+		super(message);
+	}
+	
+}
